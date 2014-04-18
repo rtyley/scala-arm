b69c22a5fb046b6b180e84a1ae3eef3903dd33a2
Roberto Tyley <roberto.tyley@gmail.com>
2014-04-18 22:35:04 +0100

    sbt-release & sbt-sonatype
    you know the drill

--- project/build.scala
+++ project/build.scala
@@ -7,21 +7,26 @@ import com.typesafe.sbt.SbtGhPages.ghpages
 import com.typesafe.sbt.SbtGit.git
 import com.typesafe.tools.mima.plugin.MimaPlugin.mimaDefaultSettings
 import com.typesafe.tools.mima.plugin.MimaKeys.previousArtifact
+import sbtrelease._
+import sbtrelease.ReleasePlugin._
+import ReleaseStateTransformations._
+import xerial.sbt.Sonatype._
+import SonatypeKeys._
+import com.typesafe.sbt.pgp.PgpKeys
 
 object ArmDef extends Build {
 
   val arm = (Project("scala-arm", file(".")) settings(
-    organization := "com.jsuereth",
+    organization := "com.madgag",
     name := "scala-arm",
-    version := "1.4-SNAPSHOT",
     scalaVersion := "2.11.0",
-    crossScalaVersions := Seq("2.9.3", "2.10.4", "2.11.0"),
+    crossScalaVersions := Seq("2.10.4", "2.11.0"),
     resolvers += "java.net repo" at "http://download.java.net/maven/2/",
     libraryDependencies ++= dependencies,
     autoCompilerPlugins := true,
     addContinuations,
     scalacOptions += "-P:continuations:enable"
-  ) settings(publishSettings:_*) settings(websiteSettings:_*)) settings(bcSettings:_*)
+  ) settings(publishSettings:_*) settings(websiteSettings:_*)) settings(bcSettings:_*) settings(releaseSettings:_*) settings(sonatypeSettings:_*)
 
   def bcSettings: Seq[Setting[_]] = mimaDefaultSettings ++ Seq(
     previousArtifact <<= scalaVersion apply { sv =>
@@ -48,7 +53,7 @@ object ArmDef extends Build {
     // Maven central wants some extra metadata to keep things 'clean'.
     homepage := Some(url("http://jsuereth.com/scala-arm")),
     licenses += "BSD-Style" -> url("http://www.opensource.org/licenses/bsd-license.php"),
-    scmInfo := Some(ScmInfo(url("http://github.com/jsuereth/scala-arm"), "scm:git@github.com:jsuereth/scala-arm.git")),
+    scmInfo := Some(ScmInfo(url("http://github.com/rtyley/scala-arm"), "scm:git@github.com:rtyley/scala-arm.git")),
     pomExtra := (
       <developers>
         <developer>
@@ -56,7 +61,30 @@ object ArmDef extends Build {
           <name>Josh Suereth</name>
           <url>http://jsuereth.com</url>
         </developer>
-      </developers>)
+      </developers>),
+    ReleasePlugin.ReleaseKeys.releaseProcess := Seq[ReleaseStep](
+      checkSnapshotDependencies,
+      inquireVersions,
+      runClean,
+      runTest,
+      setReleaseVersion,
+      commitReleaseVersion,
+      tagRelease,
+      ReleaseStep(
+        action = { state =>
+          val extracted = Project extract state
+          extracted.runAggregated(PgpKeys.publishSigned in Global in extracted.get(thisProjectRef), state)
+        },
+        enableCrossBuild = true
+      ),
+      setNextVersion,
+      commitNextVersion,
+      ReleaseStep{ state =>
+        val extracted = Project extract state
+        extracted.runAggregated(SonatypeKeys.sonatypeReleaseAll in Global in extracted.get(thisProjectRef), state)
+      },
+      pushChanges
+    )
   )
 
   def websiteSettings: Seq[Setting[_]] = (
@@ -65,7 +93,7 @@ object ArmDef extends Build {
     site.jekyllSupport() ++
     site.includeScaladoc() ++
     Seq(
-      git.remoteRepo := "git@github.com:jsuereth/scala-arm.git",
+      git.remoteRepo := "git@github.com:rtyley/scala-arm.git",
       includeFilter in Jekyll := ("*.html" | "*.png" | "*.js" | "*.css" | "CNAME")
     )
   )
